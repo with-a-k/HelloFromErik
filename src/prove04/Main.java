308d87cba7218428422a3cafacaf87a9f0bdaf48
Luke Draper <72437845+LukeDraper@users.noreply.github.com>
2020-10-07 19:53:47 -0600

    Added a greeting from Luke to Erik.

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello. My name is Erik.");
+        System.out.println("Hello Erik, my name is Luke.");
     }
 }
